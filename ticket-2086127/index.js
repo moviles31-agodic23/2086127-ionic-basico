21d07981ce46da9ba7d3fd78dbe13c008e0d5bbb
GloriaOSTOS <mygeos23@gmail.com>
2023-09-22 22:27:13 -0600

    ionic

--- ticket-2086127/index.js
+++ ticket-2086127/index.js
@@ -1,29 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const calcularBoton= document.getElementById("calcular");
-    const listaArticulos= document.getElementById("listaArticulos");
-    const totalElementos= document.getElementById("total");
+    const calcularBoton = document.getElementById("calcular");
+    const listaArticulos = document.getElementById("listaArticulos");
+    const totalElementos = document.getElementById("total");
 
-    let total=0;
+    let total = 0;
 
-    calcularBoton.addEventListener("click", function()  {
-        const articuloIngresado= document.getElementById("articulo");
-        const precioIngresado= document.getElementById("precio");
+    calcularBoton.addEventListener("click", function() {
+        const articuloIngresado = document.getElementById("articulo");
+        const precioIngresado = document.getElementById("precio");
 
         const articulo = articuloIngresado.value;
-        const precio= parseFloat(precioIngresado.value);
+        const precio = parseFloat(precioIngresado.value);
 
-        if(!isNaN(precio)){
-            const listaInter= document.createElement("li");
-            listaInter.innerHTML=  `${articulo}  +  $${precio.toFixed(2)}`;
+        if (!isNaN(precio)) {
+            const listaInter = document.createElement("li");
+            listaInter.innerHTML = `${articulo} - $${precio.toFixed(2)}`;
             listaArticulos.appendChild(listaInter);
 
             total += precio;
             totalElementos.textContent = `Total: $${total.toFixed(2)}`;
 
             articuloIngresado.value = "";
-            
             precioIngresado.value = "";
         }
     });
-
 });
